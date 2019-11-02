4d6e82a992239dee818921a1c9cf1b7e3c186697
JoyDaheeCha <daheecha0612@gmail.com>
2019-11-02 15:13:09 +0900

    Updates

---
+++ precache-manifest.bda7f793a9afabec3eed5d1886ef4b45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c503c2485ee7cdf0e23b681715256d1",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "cb5c6cafd61a207afed1",
+    "url": "/movie_app/static/css/main.95a0d1d1.chunk.css"
+  },
+  {
+    "revision": "cbc22d1e9f5e8712be39",
+    "url": "/movie_app/static/js/2.076e7f06.chunk.js"
+  },
+  {
+    "revision": "cb5c6cafd61a207afed1",
+    "url": "/movie_app/static/js/main.829fabee.chunk.js"
+  },
+  {
+    "revision": "8971f8699b511f3e262b",
+    "url": "/movie_app/static/js/runtime-main.2890315e.js"
+  }
+]);
