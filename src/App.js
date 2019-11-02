29666cb9a3e1b6ba6e1f52e9fc78e53e98c1f94e
JoyDaheeCha <daheecha0612@gmail.com>
2019-11-02 14:59:05 +0900

    Adjust styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
-    isLoading: false,
+    isLoading: true,
     movies: []
   };
   getMovies = async () => {
@@ -21,20 +22,27 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
-              <Movie
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie 
               key={movie.id}
               id={movie.id}
               year={movie.year}
               title={movie.title}
               summary={movie.summary}
-              poster={movie.medium_cover_image} />
-              )
-              )}
-      </div>
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
